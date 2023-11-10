8a8d431ffc33ea857330838746dd9840c3b89174
razbiralochka <razbiralochka@yandex.ru>
2023-11-10 11:06:15 +0400

    Init Enviroment

--- Enviroment.py
+++ Enviroment.py
@@ -1,5 +1,16 @@
 import numpy as np
-from numpy import  random
 
-class Enviroment()
-    pass
+
+class Enviroment():
+    def __init__(self):
+        #Q_table for 4 states and 4 actions
+        self.Q_table = np.random(4,4)
+        #Transition marix(4 * 4)
+        self.transotion_matrix = mp.zeros(4,4)
+        self.transotion_matrix[0,0] = -446.9
+        self.transotion_matrix[1,1] = -742.8
+        self.transotion_matrix[2,2] = -2091.0
+        self.transotion_matrix[3,3] = -(7.01+2.63+13.14)
+        self.transotion_matrix[]
+        #reward_matrix(also 4*4)
+        self.reward_matrix = np.zeros(4,4)
