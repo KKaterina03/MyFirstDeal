29bd7411375a3a3a1ab42c9fe666cfd13648a3fd
razbiralochka <razbiralochka@yandex.ru>
2023-11-19 19:25:18 +0300

    markov reward model for aging multistate system under minimal repair

---
+++ main.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+def failure_rates_func(t):
+    lam_matrx = np.zeros((4, 4))
+    lam_matrx[3][0] = 2.63
+    lam_matrx[3][1] = 7.01+0.2189*pow(t,2)
+    lam_matrx[3][2]=13.14
+    lam_matrx[0][3] = 446.9
+    lam_matrx[1][3] = 742.9
+    lam_matrx[2][3] = 2091.0
+
+    lam_matrx[0][0] = -lam_matrx[0][3]
+    lam_matrx[1][1] = -lam_matrx[1][3]
+    lam_matrx[2][2] = -lam_matrx[2][3]
+    lam_matrx[3,3] = -(lam_matrx[3][0] +  lam_matrx[3][1] + lam_matrx[3][2])
+    return lam_matrx
+
+
+def reward_func(t):
+    reward_matrx = np.zeros((4, 4))
+    reward_matrx[0][0] = 300
+    reward_matrx[1][1] = 85
+    return reward_matrx
+
+
+V = np.zeros(4)
+dV = np.zeros(4)
+time = 0
+h = 0.01;
+res = list()
+while time < 5:
+    res.append(np.mean(V))
+    for i in range(4):
+        r = reward_func(time)
+        a = failure_rates_func(time)
+        A = 0
+        B = 0
+        for j in range(4):
+            A += a[i][j]*r[i][j]
+            B += a[i][j]*V[j]
+        dV[i] = r[i][i] + A + B
+
+    V += dV * h
+    time += h
+
+plt.plot(res)
+plt.show()
+
+
+
+
+
+
