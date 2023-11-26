f3fa492bd5746153d966b46f4f2846dd751498ec
razbiralochka <razbiralochka@yandex.ru>
2023-11-26 11:28:48 +0300

    core

--- main.py
+++ main.py
@@ -1,66 +1,21 @@
-import matplotlib.pyplot as plt
+import sys
 import numpy as np
+import pyqtgraph as pg
+from PyQt5 import QtWidgets, QtCore
+from myform import Ui_MainWindow
+
+class MyWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyWindow,self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.prog)
+    def prog(self):
+        text = self.ui.lineEdit.text()
+        self.ui.label.setText("Ага, " + text)
 
 
-
-def failure_rates_func(t):
-    lam_matrx = np.zeros((4, 4))
-    lam_matrx[3,0] = 2.63
-    lam_matrx[3,1] = 7.01+0.2189*pow(t,2)
-    lam_matrx[3,2] = 13.14
-    lam_matrx[0,3] = 446.9
-    lam_matrx[1,3] = 742.9
-    lam_matrx[2,3] = 2091.0
-
-    lam_matrx[0,0] = -lam_matrx[0][3]
-    lam_matrx[1,1] = -lam_matrx[1][3]
-    lam_matrx[2,2] = -lam_matrx[2][3]
-    lam_matrx[3,3] = -(lam_matrx[3][0] + lam_matrx[3][1] + lam_matrx[3][2])
-    return lam_matrx
-
-print(failure_rates_func(0))
-def reward_func(t):
-    reward_matrx = np.zeros((4, 4))
-    reward_matrx[0,0] = 300
-    reward_matrx[1,1] = 85
-    return reward_matrx
-
-
-def equals(time_, arg):
-    r = reward_func(time_)
-    a = failure_rates_func(time_)
-    dV = np.zeros(4)
-    for i in range(4):
-        A = 0
-        B = 0
-        for j in range(4):
-            if j != i:
-                A += a[i][j]*r[i][j]
-            B += a[i][j]*arg[j]
-
-        dV[i] = r[i][i] + A + B
-
-    return dV
-
-
-V = np.zeros(4)
-time = 0
-h = 0.001;
-res = list()
-timeL = list()
-
-
-
-while time < 5:
-    k1 = equals(time, V)
-    k2 = equals(time+h/2, V + k1* h/2)
-    k3 = equals(time + h / 2, V + k2 * h / 2)
-    k4 = equals(time + h, V + k3 * h)
-
-    V += (k1+k2+k3+k4)*h/6
-    time += h
-    res.append(V.mean())
-    timeL.append(time)
-
-plt.plot(timeL, res)
-plt.show()
+app = QtWidgets.QApplication([])
+application = MyWindow()
+application.show()
+sys.exit(app.exec())
---
+++ myform.py
@@ -0,0 +1,44 @@
+
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(266, 171)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(50, 20, 160, 81))
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.lineEdit = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout.addWidget(self.lineEdit)
+        self.pushButton = QtWidgets.QPushButton(self.verticalLayoutWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout.addWidget(self.pushButton)
+        self.label = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 266, 22))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.label.setText(_translate("MainWindow", "TextLabel"))
