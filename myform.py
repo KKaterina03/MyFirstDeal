8d2134ff0b5d17aadf631a57ac844f81c08f105b
razbiralochka <razbiralochka@yandex.ru>
2023-11-26 13:22:03 +0300

    core

--- myform.py
+++ myform.py
@@ -39,6 +39,6 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "PushButton"))
-        self.label.setText(_translate("MainWindow", "TextLabel"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Я в гитхабe"))
+        self.pushButton.setText(_translate("MainWindow", "Тыкни на меня"))
+        self.label.setText(_translate("MainWindow", "Вот тут будет слово"))
